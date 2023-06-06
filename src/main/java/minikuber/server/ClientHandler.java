b29148c836d5a6facfcbbe21de145a57146dc042
sinamhdv <sinamhdv@gmail.com>
2023-06-06 21:33:15 +0330

    completed client handler connection log

--- src/main/java/minikuber/server/ClientHandler.java
+++ src/main/java/minikuber/server/ClientHandler.java
@@ -1,12 +1,13 @@
 package minikuber.server;
 
+import java.io.IOException;
 import java.net.Socket;
 
 import minikuber.shared.LogType;
 import minikuber.shared.Utils;
 
 public class ClientHandler extends Thread {
-	private Socket socket;
+	private final Socket socket;
 
 	public ClientHandler(Socket socket) {
 		this.socket = socket;
@@ -14,6 +15,11 @@ public class ClientHandler extends Thread {
 
 	@Override
 	public void run() {
-		Utils.log(LogType.INFO, "New connection from " + socket.getInetAddress() + ":" + socket.getPort());
+		try {
+			Utils.log(LogType.INFO, "New connection from " + socket.getInetAddress() + ":" + socket.getPort());
+			socket.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
 	}
 }
--- src/main/java/minikuber/server/ServerMain.java
+++ src/main/java/minikuber/server/ServerMain.java
@@ -1,8 +1,10 @@
 package minikuber.server;
 
 public class ServerMain {
+	private static final int SERVER_PORT = 12345;
+
 	public static void run() throws Exception {
 		System.out.println("======[minikuber server]======");
-		new Server(12345).serve();
+		new Server(SERVER_PORT).serve();
 	}
 }
